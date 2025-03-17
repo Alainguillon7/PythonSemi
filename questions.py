425623c4d6f8a9d11758d17fbf38cdcd86540739
Alain Guillon <alainguillon01@gmail.com>
2025-03-17 19:19:47 -0300

    P3- Deja de acceder a las 3 listas medinate indice

--- questions.py
+++ questions.py
@@ -29,15 +29,16 @@ puntaje = 0
 # Índice de la respuesta correcta para cada pregunta, en el mismo orden que las preguntas
 correct_answers_index = [1, 2, 0, 3, 1]
 
-# El usuario deberá contestar 3 preguntas
-for _ in range(3):
-    # Se selecciona una pregunta aleatoria
-    question_index = random.randint(0, len(questions) - 1)
+# Se genera una lista que contenga 3 de las preguntas,respuestas posibles y respuesta correcta de manera que no haya preguntas repetidas
+questions_to_ask = random.choices(
+    list(zip(questions, answers, correct_answers_index)), k=3
+)
 
-    # Se muestra la pregunta y las respuestas posibles
-    print(questions[question_index])
-    for i, answer in enumerate(answers[question_index]):
-        print(f"{i + 1}. {answer}")
+# El usuario deberá contestar 3 preguntas
+for question, options, answer in questions_to_ask:
+    print(question)
+    for i, option in enumerate(options):
+        print(f"{i + 1}. {option}")
 
     # El usuario tiene 2 intentos para responder correctamente
     for intento in range(2):
@@ -50,7 +51,7 @@ for _ in range(3):
             print(f"Respuesta no válida. Debe ingresar un numero entre 1 y 4")
             sys.exit(1)
         # Se verifica si la respuesta es correcta
-        if user_answer == correct_answers_index[question_index]:
+        if user_answer == answer:
             print("¡Correcto!")
             puntaje += 1
             break
@@ -61,7 +62,7 @@ for _ in range(3):
             # Si es el segundo intento, se muestra la respuesta correcta
             if intento == 1:
                 print("La respuesta correcta es:")
-                print(answers[question_index][correct_answers_index[question_index]])
+                print(options[answer])
 
     # Se imprime un espacio en blanco al final de la pregunta
     print()
